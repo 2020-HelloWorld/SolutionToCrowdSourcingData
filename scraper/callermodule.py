9c78cbab0c389157ca88756c8f898a0bcfe2c42a
Ghanashyam Bhat <74894094+Ghanashyam-Bhat@users.noreply.github.com>
2023-02-05 15:21:53 +0530

    uncomment filtering

--- scraper/callermodule.py
+++ scraper/callermodule.py
@@ -1,4 +1,5 @@
 from Blackbird_usecase import scrapeBlackBird
 import filter_black_bird
-#scrapeBlackBird("elonmusk")
-filter_black_bird.filter_BB("elonmusk")
+username = "the.giga.byte"
+scrapeBlackBird(username)
+filter_black_bird.filter_BB(username)
