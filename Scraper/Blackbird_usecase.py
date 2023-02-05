f61c0d107301b0e9ead74a7a915a2ae611dfd013
Ghanashyam Bhat <74894094+Ghanashyam-Bhat@users.noreply.github.com>
2023-02-05 10:00:26 +0530

    Delete Scraper directory

--- Scraper/Blackbird_usecase.py
+++
@@ -1,7 +0,0 @@
-import os
-def scrapeBlackBird(username):
-    try:
-        os.system("python blackbird.py -u "+username)
-        return 0
-    except:
-        return -1
--- Scraper/callermodule.py
+++
@@ -1,4 +0,0 @@
-from Blackbird_usecase import scrapeBlackBird
-import filter_black_bird
-#scrapeBlackBird("elonmusk")
-filter_black_bird.filter_BB("elonmusk")
